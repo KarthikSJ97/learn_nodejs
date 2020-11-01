f243f0fb7233c23bb02e8ce241fe0169e256dd99
KarthikSJhingade <karthik.jhingade@nineleaps.com>
2020-11-02 00:18:55 +0530

    [lib.js] | tests | unit test for getCurrencies function (arrays)

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -24,3 +24,27 @@ describe('greet', () => {
         expect(result).toContain('Karthik');
     });
 });
+
+describe('getCurrencies', () => {
+    it('should return supported currencies', () => {
+        const result = lib.getCurrencies();
+        // Too general
+        expect(result).toBeDefined();
+        expect(result).not.toBeNull();
+
+        // Too specific
+        expect(result[0]).toBe('USD');
+        expect(result[1]).toBe('AUD');
+        expect(result[2]).toBe('EUR');
+
+        expect(result.length).toBe(3);
+
+        // Proper way
+        expect(result).toContain('USD');
+        expect(result).toContain('AUD');
+        expect(result).toContain('EUR');
+
+        // Ideal way
+        expect(result).toEqual(expect.arrayContaining(['USD', 'EUR', 'AUD']));
+    });
+});
