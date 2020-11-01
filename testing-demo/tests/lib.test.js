0478fe59e2b340a18b86a48d6846e486bbafc2d9
KarthikSJhingade <karthik.jhingade@nineleaps.com>
2020-11-02 00:09:15 +0530

    [lib.js] | tests | unit test for greeting function (strings)

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -6,7 +6,7 @@ describe('absolute', () => {
         expect(result).toBe(1);
     });
     
-    it('should return a negative number if input is negative', () => {
+    it('should return a positive number if input is negative', () => {
         const result = lib.absolute(-1);
         expect(result).toBe(1);
     });
@@ -14,5 +14,13 @@ describe('absolute', () => {
     it('should return 0 when input is 0', () => {
         const result = lib.absolute(0);
         expect(result).toBe(0);
+    });
+});
+
+describe('greet', () => {
+    it('should return a greeting message', () => {
+        const result = lib.greet('Karthik');
+        expect(result).toMatch(/Karthik/);
+        expect(result).toContain('Karthik');
     });
 });
