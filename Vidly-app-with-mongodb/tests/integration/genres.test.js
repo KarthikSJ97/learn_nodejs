57d33c313e386d4f7139f48aabf826114474fb19
KarthikSJhingade <karthik.jhingade@nineleaps.com>
2020-11-03 00:43:44 +0530

    [genres.test.js] | tests | Generating coverage using jest

--- Vidly-app-with-mongodb/tests/integration/genres.test.js
+++ Vidly-app-with-mongodb/tests/integration/genres.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const {Genre} = require('../../models/genre');
 const {User} = require('../../models/user');
+const mongoose = require('mongoose');
 let server;
 
 describe('/api/genres', () => {
@@ -42,6 +43,13 @@ describe('/api/genres', () => {
 
         it('should return 404 if invalid id is passed', async() => {
             const res = await request(server).get('/api/genres/1');
+
+            expect(res.status).toBe(404);
+        });
+
+        it('should return 404 if no genre with given id exist', async() => {
+            const id = mongoose.Types.ObjectId();
+            const res = await request(server).get('/api/genres/'+id);
 
             expect(res.status).toBe(404);
         });
